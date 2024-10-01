139d778d54ae689c9d0d547d2fea8dd72c100e9e
Martin Leong <martinleongmo@gmail.com>
2024-10-02 06:31:55 +0800

    Update CalculateShape.java

--- a_dependency_challenge_answer/src/CalculateShape.java
+++ a_dependency_challenge_answer/src/CalculateShape.java
@@ -1,16 +1,13 @@
 public class CalculateShape {
-
     Shape shape;
-
     public CalculateShape(Shape shape){
         this.shape = shape;
     }
-
     public void PrintArea(){
-        System.out.println(this.shape.area());
+        System.out.println(String.format("%.2f", this.shape.area()));
     }
-
     public void PrintPerimeter(){
-        System.out.println(this.shape.perimeter());
+        System.out.println(String.format("%.2f", this.shape.perimeter()));
     }
 }
+
