2e00e09eb39083acd4018220b178ae2fd78d46a2
Leong Martin <leongmartin@Leongs-MacBook-Air.local>
2024-10-04 10:52:44 +0800

    added validations for Customer class

--- c_jpa_service/src/main/java/org/example/jpa/model/Customer.java
+++ c_jpa_service/src/main/java/org/example/jpa/model/Customer.java
@@ -1,6 +1,7 @@
 package org.example.jpa.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 import java.util.List;
 
@@ -9,15 +10,21 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Min(value = 1, message = "ID must be a positive number")
     private Long id;
 
     @Column (nullable = false)
+    @NotBlank(message = "Name cannot be blank.")
+    @Size(min = 3, message = "Name must be at least 3 characters.")
+    @Size(max = 255, message = "Name must not be more than 255 characters.")
     private String name;
 
-    @Column (nullable = false)
+    @Column (nullable = false, unique = true)
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}", message = "Email is not valid.")
     private String email;
 
     @Column
+    @Pattern(regexp = "^\\d{8}$", message = "Phone number must be 8-digits only.")
     private String phone;
 
     public Customer() {     // create a new empty instance of Customer
