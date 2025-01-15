29bdc49349ea1c52e132522606ada9d0c9122293
Martin Leong <martinleongmo@gmail.com>
2025-01-15 18:21:50 +0800

    Update CustomerRepository.java

--- b_jpa/src/main/java/com/sctp/fsd/jpa/repository/CustomerRepository.java
+++ b_jpa/src/main/java/com/sctp/fsd/jpa/repository/CustomerRepository.java
@@ -4,12 +4,17 @@ import com.sctp.fsd.jpa.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
-    // By extending this Repository with CrudRepository, these default methods are implemented
-    // save()
-    // findOne()
-    // findById()
-    // findAll()
-    // count()
-    // delete()
-    // deleteById()
+    // By extending this repository with CrudRepository
+    // the methods from CrudRepository can be used by CustomerRepository
+
+    // save()                - save and update operation
+    // findById(Integer)     - find an instance by Id
+    // findAll()             - find all instances of records
+    // count()               - count all instances of records
+    // delete(object)        - delete an instance
+    // delete(Integer)       - delete an instance by Id
+    // for all the methods, refer to CrudRepository interface
+
+    // Create custom query
+    List<Customer> findByEmailContainingOrLastNameContaining(String email, String lastName);   // SELECT * from customer WHERE email LIKE "%email%" OR lastName LIKE "%lastName%";
 }
